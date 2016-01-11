c8ce87e4237e03a260f41ec63cde57ecb2ca39ec
Emmanuel Courcelle <emmanuel.courcelle@laposte.net>
2016-01-11 08:58:07 +0100

    install.sh utilise maintenant ssh

--- install.sh
+++ install.sh
@@ -1,23 +1,24 @@
 #! /bin/bash
 
-[ "$1" = "" ] && echo "merde" && exit 1
+[ "$1" = "" ] && echo "Usage: ./install.sh eoslogin1 /users/sysadmin/$(whoami)" && exit 1
 
-DST=$1
+EOS=$1
+DST=$2
 BIN="$DST/bin"
 LIB="$DST/lib/placement"
 
-[ ! -d $BIN ] && echo "Pas de répertoire $BIN" && exit 1
-[ ! -d $LIB ] && echo "Pas de répertoire $LIB" && exit 1
+ssh $EOS "[ ! -d $BIN ] && echo Pas de répertoire $BIN sur $EOS" && exit 1
+ssh $EOS "[ ! -d $LIB ] && echo Pas de répertoire $LIB sur $EOS" && exit 1
 
 SRC=usr/local
 
 for f in hardware.py architecture.py exception.py tasksbinding.py scatter.py compact.py running.py utilities.py
 do
-  cp $SRC/lib/placement/$f $LIB
+  scp $SRC/lib/placement/$f "$EOS:$LIB"
 done
 
-cp $SRC/bin/placement.py $SRC/bin/placement $BIN/
-chmod a=r,u+w $LIB/*
-chmod a=rx,u+w $BIN/placement.py $BIN/placement
+scp $SRC/bin/placement.py $SRC/bin/placement "$EOS:$BIN"
+ssh $EOS "chmod a=r,u+w $LIB/*"
+ssh $EOS "chmod a=rx,u+w $BIN/placement.py $BIN/placement"
 
 echo "C'est fait, man !"
